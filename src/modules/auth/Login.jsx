ac0686fefc513392c6d60705c1c4158b6dc65e5d
ajaykandarkar <ajaykandarkar170@gmail.com>
2025-04-03 17:23:27 +0530

    changes env file

--- src/modules/auth/Login.jsx
+++ src/modules/auth/Login.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { TopBar } from '../../componenets/TopBar'
+import axios from 'axios';
+
+  const handleLogin = () =>{
+    const uri = process.env.REACT_APP_BASE_URL
+    axios
+    .post(`${uri}/login`)
+    .then((response) => {
+      console.log("response of ",response)
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
+  }
 
 export const Login = () => {
   return (
     <div>
         <TopBar/>
+      <button  onClick={handleLogin}>Login click</button>
     </div>
   )
 }
+
