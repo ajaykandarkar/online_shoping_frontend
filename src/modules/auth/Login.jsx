600e0cf7f00ac8eec05541eeb4897e11ca9b47c3
ajaykandarkar <ajaykandarkar170@gmail.com>
2025-04-03 17:32:00 +0530

    remove unused

--- src/modules/auth/Login.jsx
+++ src/modules/auth/Login.jsx
@@ -1,18 +1,17 @@
-import React, { useEffect } from 'react'
 import { TopBar } from '../../componenets/TopBar'
 import axios from 'axios';
 
-  const handleLogin = () =>{
-    const uri = process.env.REACT_APP_BASE_URL
-    axios
-    .post(`${uri}/login`)
-    .then((response) => {
-      console.log("response of ",response)
-    })
-    .catch((error) => {
-      console.error('Error fetching data:', error);
-    });
-  }
+const handleLogin = () =>{
+  const uri = process.env.REACT_APP_BASE_URL
+  axios
+  .post(`${uri}/login`)
+  .then((response) => {
+    console.log("response of ",response)
+  })
+  .catch((error) => {
+    console.error('Error fetching data:', error);
+  });
+}
 
 export const Login = () => {
   return (
